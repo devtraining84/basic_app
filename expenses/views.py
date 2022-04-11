8f02c1484bbfc6caa2b316c27c37ab4eb00d8149
devtraining84 <dev.training.84@gmail.com>
2022-04-11 22:00:09 +0200

    Task 4.4 is running

--- expenses/views.py
+++ expenses/views.py
@@ -2,10 +2,13 @@ from datetime import date
 from unicodedata import category
 from urllib import request
 from django.views.generic.list import ListView
+
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
 #from django.db.models import Q
 from .reports import summary_per_category
+from django.db.models import Sum
+
 
 
 class ExpenseListView(ListView):
@@ -15,7 +18,7 @@ class ExpenseListView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
         form = ExpenseSearchForm(self.request.GET)
-        
+        sum = queryset.aggregate(Sum('amount')).get('amount__sum') 
         
         
         if form.is_valid():
@@ -26,9 +29,6 @@ class ExpenseListView(ListView):
             amount_min = form.data.get('amount_min')
             amount_max = form.data.get('amount_max')
             select = form.data.get('select')
-
-
-
 
             if name:
                 queryset = queryset.filter(name__icontains=name)
@@ -44,14 +44,25 @@ class ExpenseListView(ListView):
                 queryset = queryset.filter(amount__lte=amount_max)            
             if select:
                 queryset = queryset.order_by(select)
-
+            
+            
+            
+            
+            
             return super().get_context_data(
             form=form,
             object_list=queryset,
+            sum = sum,
             summary_per_category=summary_per_category(queryset),
             **kwargs)
 
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
+
+
+
+    
+
+
 
