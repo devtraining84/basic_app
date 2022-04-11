e22816c60b2ecfde849000266547358fd5176600
devtraining84 <dev.training.84@gmail.com>
2022-04-11 19:47:54 +0200

    4.1 and 4.2 are running

--- expenses/forms.py
+++ expenses/forms.py
@@ -5,9 +5,12 @@ from .models import Expense
 class ExpenseSearchForm(forms.ModelForm):
     class Meta:
         model = Expense
-        fields = ('name', 'date')
+        fields = ['name' ,'category']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
-        self.fields['date'].required = False
+        self.fields['category'].required = False
+       
+
+
--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,10 @@
 from datetime import date
+from unicodedata import category
+from urllib import request
 from django.views.generic.list import ListView
-
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
+#from django.db.models import Q
 from .reports import summary_per_category
 
 
@@ -12,16 +14,36 @@ class ExpenseListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
-
         form = ExpenseSearchForm(self.request.GET)
+        
+        
+        
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
-            date_form = form.cleaned_data.get('date', '')
+            form_category = form.cleaned_data.get('category', '')
+            date_from = form.data.get('date_from')
+            date_to = form.data.get('date_to')
+            amount_min = form.data.get('amount_min')
+            amount_max = form.data.get('amount_max')
+            
+
+
+
             if name:
                 queryset = queryset.filter(name__icontains=name)
-            if date_form:
-                queryset = queryset.filter(date__icontains=date_form)
-        return super().get_context_data(
+            if form_category:
+                queryset = queryset.filter(category=form_category)
+            if date_from:
+                queryset = queryset.filter(date__gte=date_from)
+            if date_to:
+                queryset = queryset.filter(date__lte=date_to)
+            if amount_min:
+                queryset = queryset.filter(amount__gte=amount_min)
+            if amount_max:
+                queryset = queryset.filter(amount__lte=amount_max)            
+
+
+            return super().get_context_data(
             form=form,
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
